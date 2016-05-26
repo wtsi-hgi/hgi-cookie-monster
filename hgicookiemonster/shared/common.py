9b0adeea99e331622ba8302cdcf11cb6a7cd1cd6
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-05-26 13:08:11 +0100

    Generalised method to decide if lastest iRODS update indicates the related iRODS entity is part of a given study.

--- hgicookiemonster/shared/common.py
+++ hgicookiemonster/shared/common.py
@@ -1,6 +1,10 @@
 from cookiemonster.common.models import Cookie
+from cookiemonster.retriever.source.irods.json import DataObjectModificationJSONDecoder
+from cookiemonster.retriever.source.irods.models import DataObjectModification
 from hgicookiemonster.enrichment_loaders._irods import IRODS_ENRICHMENT
 from hgicookiemonster.run import IRODS_UPDATE_ENRICHMENT
+
+_STUDY_ID_KEY = "study_id"
 
 
 def has_irods_update_enrichment_followed_by_irods_enrichment(cookie: Cookie) -> bool:
@@ -17,3 +21,26 @@ def has_irods_update_enrichment_followed_by_irods_enrichment(cookie: Cookie) -> 
 
     if not (irods_update_enrichment.source == IRODS_UPDATE_ENRICHMENT and irods_enrichment.source == IRODS_ENRICHMENT):
         return False
+
+
+def study_with_id_in_latest_irods_update(id: str, cookie: Cookie) -> bool:
+    """
+    Decides whether the Cookie shows that metadata was added in the iRODS update to indicate that the updated iRODS
+    entity belongs to the study with the given ID.
+    :param id: the ID of the study of interest
+    :param cookie: the cookie
+    :return: whether the last iRODS update shows that the related iRODS entity is part of the study with the given ID
+    """
+    irods_update_enrichment = cookie.get_most_recent_enrichment_from_source(IRODS_UPDATE_ENRICHMENT)
+
+    if irods_update_enrichment is None:
+        return False
+
+    data_object_modification = DataObjectModificationJSONDecoder().decode(
+        irods_update_enrichment.metadata)  # type: DataObjectModification
+    modified_metadata = data_object_modification.modified_metadata
+
+    if _STUDY_ID_KEY not in modified_metadata:
+        return False
+
+    return id in modified_metadata[_STUDY_ID_KEY]
