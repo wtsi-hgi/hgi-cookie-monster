f587d91cdcc4271e51bf0b73d338c5bffacbd9ad
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-06-06 11:46:10 +0100

    Made file names more human readable.

--- hgicookiemonster/enrichment_loaders/study_page_loader.py
+++ hgicookiemonster/enrichment_loaders/study_page_loader.py
@@ -7,9 +7,10 @@ from hgicookiemonster.enrichment_loaders._irods import load_enrichment_from_irod
 from hgicookiemonster.shared.common import study_with_id_in_most_recent_irods_update
 
 
+
 def _can_enrich(cookie: Cookie, context: HgiContext) -> bool:
     return cookie.enrichments[-1].source != IRODS_ENRICHMENT and study_with_id_in_most_recent_irods_update("3543", cookie)
 
 
-_enrichment_loader = EnrichmentLoader(_can_enrich, load_enrichment_from_irods, priority=0, name=IRODS_ENRICHMENT)
+_enrichment_loader = EnrichmentLoader(_can_enrich, load_enrichment_from_irods, priority=0, id=IRODS_ENRICHMENT)
 register(_enrichment_loader)
